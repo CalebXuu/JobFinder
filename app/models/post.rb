368512df6a8eb8869af257da9a50bc8785127c2d
jjot <jjot@github.com>
2017-11-19 12:24:16 -0800

    simple search bar

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,5 @@
 class Post < ActiveRecord::Base
+  def self.search(search)
+    where("job_title ILIKE ? OR company ILIKE ? OR location ILIKE ?", "%#{search}%","%#{search}%","%#{search}%")
+  end
 end
